c3c47c774ed19c0568bdc25e2dd5eca998b2cc2c
4sizn <4sizn@naver.com>
2024-06-29 01:09:54 +0900

    chore: .

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import "./style.css";
+
+function saveToFile(element: HTMLButtonElement) {
+  element.addEventListener("click", () => {
+    const blob = new Blob(["Hello, world1!"], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "hello.txt";
+    a.click();
+    URL.revokeObjectURL(url);
+  });
+}
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <div class="card">
+    <button id="save" type="button">Save</button>
+    </div>
+
+  </div>
+`;
+
+saveToFile(document.querySelector<HTMLButtonElement>("#save")!);
